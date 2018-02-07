75fece0bfe853a5b9d52decf72c4876cdae8d336
Arthur Dassier <arthur.dassier@epitech.eu>
2018-02-07 18:38:55 +0100

    .gitignore created, minimal error gest

--- srcs/brain.c
+++ srcs/brain.c
@@ -7,7 +7,7 @@
 
 #include "navy.h"
 
-int main(int ac, char **av)
+void play(int ac, char **av)
 {
 	char	**enemys_map = malloc(sizeof(char*) * 11);
 	char	**my_map = malloc(sizeof(char*) * 11);
@@ -16,18 +16,31 @@ int main(int ac, char **av)
 
 	ships_infos(ac, av, ships);
 	while (ships[i] != NULL)
-		printf("%s\n", ships[i++]);
+	my_printf("%s\n", ships[i++]);
 	i = 0;
 	empty_map(enemys_map);
 	ships_map(my_map, ships);
-	printf("\nmy position:\n");
+	my_printf("\nmy position:\n");
 	while (my_map[i] != NULL)
-		printf("%s\n", my_map[i++]);
+	printf("%s\n", my_map[i++]);
 	i = 0;
-	printf("\nenemy's position:\n");
+	my_printf("\nenemy's position:\n");
 	while (enemys_map[i] != NULL)
-		printf("%s\n", enemys_map[i++]);
+	my_printf("%s\n", enemys_map[i++]);
 	free(my_map);
 	free(enemys_map);
+}
+
+int main(int ac, char **av)
+{
+	if (ac < 2) {
+		my_puterror("You need a folder as argument\n");
+		return (84);
+	}
+	if (ac > 2) {
+		my_puterror("You must give only one folder as argument\n");
+		return (84);
+	}
+	play(ac, av);
 	return(0);
 }
