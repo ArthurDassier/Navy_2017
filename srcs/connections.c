597615ab72024a46b218c367320d719b82454647
Arthur Dassier <arthur.dassier@epitech.eu>
2018-02-08 14:32:37 +0100

    Corrected display

--- srcs/connections.c
+++ srcs/connections.c
@@ -16,10 +16,10 @@ void wait_connection(int ac, char *av[])
 {
 	struct sigaction act;
 
-	my_printf("pid : %d\n", getpid());
-	my_printf("Waiting for the ennemy to connect ...\n");
+	my_printf("my_pid:\t%d\n", getpid());
+	my_printf("waiting for enemy connection...\n");
 	act.sa_sigaction = &catch_sigint;
-	while (i == 1) {
+	while (1) {
 		if (sigaction(SIGUSR1, &act, NULL))
 			break;
 	}
@@ -32,7 +32,8 @@ int server(int ac, char *av[])
 		wait_connection(ac, av);
 	if (ac == 3) {
 		kill(my_getnbr(av[1]), SIGUSR1);
-		my_printf("connected to server\n");
+		my_printf("my_pid:\t%d\n", getpid());
+		my_printf("successfully connected\n");
 		av[1] = av[2];
 	}
 	play(ac, av);
