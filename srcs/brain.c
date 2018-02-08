670e1bd5634eb33f5f38f7ce5f57d0f27bc1452f
Arthur Dassier <arthur.dassier@epitech.eu>
2018-02-08 16:14:31 +0100

    Added a fonction to keep the pids somewhere

--- srcs/brain.c
+++ srcs/brain.c
@@ -42,5 +42,5 @@ int main(int ac, char **av)
 		return (84);
 	}
 	server(ac, av);
-	return(0);
+	return (0);
 }
--- srcs/connections.c
+++ srcs/connections.c
@@ -9,6 +9,22 @@
 
 static int global = 0;
 
+int keep_pid(int usr, int pid)
+{
+	static int pid_usr1 = 0;
+	static int pid_usr2 = 0;
+
+	if (usr == 1)
+		pid_usr1 = pid;
+	else if (usr == 2)
+		pid_usr2 = pid;
+	else if (usr == 3)
+		return (pid_usr1);
+	else
+		return (pid_usr2);
+	return (-1);
+}
+
 void catch_sigint(int sig, siginfo_t *siginfo, void *context)
 {
 	my_printf("ennemy connected\n");
@@ -20,6 +36,7 @@ void wait_connection()
 	struct sigaction act;
 
 	my_printf("my_pid:\t%d\n", getpid());
+	keep_pid(1, getpid());
 	my_printf("waiting for enemy connection...\n");
 	act.sa_sigaction = &catch_sigint;
 	sigaction(SIGUSR1, &act, NULL);
@@ -33,6 +50,7 @@ int server(int ac, char *av[])
 	if (ac == 3) {
 		kill(my_getnbr(av[1]), SIGUSR1);
 		my_printf("my_pid:\t%d\n", getpid());
+		keep_pid(2, getpid());
 		my_printf("successfully connected\n");
 		av[1] = av[2];
 	}
