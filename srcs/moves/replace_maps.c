f73a00497aa69b9440e90ba0553b1f47879aa7f1
Vincent <vincent.lemesle@epitech.eu>
2018-02-12 15:58:27 +0100

    patch

---
+++ srcs/moves/replace_maps.c
@@ -0,0 +1,13 @@
+/*
+** EPITECH PROJECT, 2017
+** replace_maps.c
+** File description:
+** replace_maps_with_info
+*/
+
+#include "navy.h"
+
+maps *replace_maps(maps *navy_maps, structe col_lin)
+{
+	
+}
