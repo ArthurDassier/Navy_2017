8691a549add8a4bf66a656a2ed3653d1a0735f87
Arthur Dassier <arthur.dassier@epitech.eu>
2018-02-23 13:52:03 +0100

    Added -h

--- srcs/brain.c
+++ srcs/brain.c
@@ -7,6 +7,17 @@
 
 #include "navy.h"
 
+void helper(void)
+{
+	my_printf("USAGE\n\t");
+	my_printf("./navy [first_player_pid] navy_positions\n");
+	my_printf("DESCRIPTION\n\t");
+	my_printf("first_player_pid:  only for the 2nd player.");
+	my_printf(" pid of the first player.\n");
+	my_printf("\tnavy_positions:  file representing ");
+	my_printf("the positions of the ships.\n");
+}
+
 int main(int ac, char **av)
 {
 	maps	*navy_maps = malloc(sizeof(maps));
@@ -14,6 +25,10 @@ int main(int ac, char **av)
 	if (ac > 3 || ac < 2) {
 		my_puterror("bad argument\n");
 		return (84);
+	}
+	if (av[1][0] == '-' && av[1][1] == 'h') {
+		helper();
+		return (0);
 	}
 	init_maps(navy_maps, ac, av);
 	return (server(ac, av, navy_maps));
