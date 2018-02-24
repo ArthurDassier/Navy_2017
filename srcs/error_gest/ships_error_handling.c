c37ae6c07a053b95c044c9838e83fabaa236378c
Arthur Dassier <arthur.dassier@epitech.eu>
2018-02-24 16:14:59 +0100

    To check old v

--- srcs/error_gest/ships_error_handling.c
+++ srcs/error_gest/ships_error_handling.c
@@ -12,8 +12,8 @@ int ships_nb_handling(char **ships)
 	int	i = 0;
 
 	while (ships[i]) {
-		if (ships[i][0] != '2' && ships[i][0] != '3' &&
-				ships[i][0] != '4' && ships[i][0] != '5')
+		if (ships[i][0] != '2' && ships[i][0] != '3'
+		&& ships[i][0] != '4' && ships[i][0] != '5')
 			return (-1);
 		++i;
 	}
@@ -22,8 +22,8 @@ int ships_nb_handling(char **ships)
 
 int ships_size_handling(char **ships)
 {
-	if (ships[0][0] == ships[1][0] || ships[0][0] == ships[2][0] ||
-			ships[0][0] == ships[3][0])
+	if (ships[0][0] == ships[1][0] || ships[0][0] == ships[2][0]
+	|| ships[0][0] == ships[3][0])
 		return (-1);
 	else if (ships[1][0] == ships[2][0] || ships[1][0] == ships[3][0])
 		return (-1);
@@ -36,7 +36,7 @@ int ships_error_handling(char **ships)
 {
 	if (ships_nb_handling(ships) != 0)
 		return (-1);
-	else if (ships_size_handling(ships) != 0)
-		return (-1);
+	//else if (ships_size_handling(ships) != 0)
+		//return (-1);
 	return (0);
 }
